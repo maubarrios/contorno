92cd021d43bef12a0bdbefce3429188d00c08409
maubarrios <mauricio.barrios@gmail.com>
2020-09-20 14:10:36 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,73 @@
+basic.forever(function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . # # #
+        . # . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . # #
+        . . # . #
+        . # . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . # . #
+        . # . . #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # #
+        . . # . #
+        . # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . # #
+        . . # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        `)
+})
